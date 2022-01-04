f0dfc9c53484c49ef58f883662a60a98f2441510
taher33 <taherhehe@gmail.com>
2022-01-05 00:27:07 +0100

    new private message feature

--- routes/handlechat.js
+++ routes/handlechat.js
@@ -18,7 +18,7 @@ module.exports = (io, socket, client) => {
       let reciever;
       if (`${thread.clients[0]}` === sender) reciever = thread.clients[1];
       else reciever = thread.clients[0];
-      console.log(`${thread.clients[0]}` === sender);
+
       let msg = {
         sender,
         reciever,
@@ -60,6 +60,32 @@ module.exports = (io, socket, client) => {
     }
   }
 
+  const newPrivateMessage = async (payload, cb) => {
+    const { sender, reciever, content } = payload;
+    if (!reciever || !sender || !content) return cb({ status: "error" });
+    try {
+      const client = await User.findById(sender);
+      const partner = await User.findById(reciever);
+      if (!client || !partner) return cb({ status: "error" });
+      const message = await Messages.create({ sender, content, reciever });
+      const thread = await Thread.create({
+        clients: [sender, reciever],
+        messages: [message],
+      });
+
+      client.threads.push(thread._id);
+      partner.threads.push(thread._id);
+      client.save({ validateBeforeSave: false });
+      partner.save({ validateBeforeSave: false });
+
+      cb({ status: "success" });
+    } catch (err) {
+      console.log(err);
+      const error = handleError(err);
+      cb({ status: "error", error });
+    }
+  };
+
   const getMessages = async (payload, cb) => {
     const { threadId } = payload;
     if (!threadId)
@@ -74,5 +100,6 @@ module.exports = (io, socket, client) => {
   };
 
   socket.on("private message", privateMessage);
+  socket.on("new private message", newPrivateMessage);
   socket.on("get previous messages", getMessages);
 };
